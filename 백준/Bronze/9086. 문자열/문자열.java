b4761b4eee0bc177c49c02412a7759a2deb5cd4e
RangKoon <162738215+RangKoon@users.noreply.github.com>
2024-03-25 12:22:10 +0900

    [Bronze V] Title: 문자열, Time: 104 ms, Memory: 12764 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,13 +1,12 @@
-import java.util.*;
-public class Main{
-
-     public static void main(String []args){
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-       int T = sc.nextInt();
-       String[] arr = new String[T];
-       for(int i=0;i<T;i++){
-           arr[i]=sc.next();
-           System.out.println(arr[i].charAt(0)+""+arr[i].charAt(arr[i].length()-1));
-       }
-     }
+        int t = Integer.parseInt(sc.nextLine());
+        for(int i=0; i<t; i++) {
+        	String str = sc.nextLine();
+        	
+        	System.out.println(str.charAt(0)+""+str.charAt(str.length()-1));
+        }
+    }
 }
