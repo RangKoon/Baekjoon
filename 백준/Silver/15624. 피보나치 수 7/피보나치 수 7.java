423c094e320f59a1b99328434e2849b0957c61bb
RangKoon <162738215+RangKoon@users.noreply.github.com>
2024-03-31 22:20:26 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 128 ms, Memory: 20700 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -4,13 +4,13 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int arr[] = new int[1_000_001];
+		long arr[] = new long[1_000_001];
 		arr[0] = 0;
 		arr[1] = 1;
 		
 		
 		for(int i=2; i<=n; i++) {
-			arr[i] = arr[i-1]%1_000_000_007 + arr[i-2]%1_000_000_007;
+			arr[i] = (arr[i-1] + arr[i-2])%1_000_000_007;
 		}
 		System.out.println(arr[n]);
 
