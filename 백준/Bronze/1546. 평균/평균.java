eea9039663ff02661c7f23807a0e9c6c3e4c0a7c
RangKoon <162738215+RangKoon@users.noreply.github.com>
2024-04-02 20:34:29 +0900

    [Bronze I] Title: 평균, Time: 152 ms, Memory: 14808 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,22 +1,24 @@
-import java.util.*;
-public class Main{
-
-     public static void main(String []args){
-        Scanner sc=new Scanner(System.in);
-        
-        int N = sc.nextInt();
-        float[] grade = new float[N];
-        float max = 0;
-        float sum = 0;
-        for(int i = 0; i<N; i++){
-            grade[i] = sc.nextFloat();
-            if(grade[i]>max)
-            max = grade[i];
-        }
-        for(int i = 0; i<N; i++){
-            grade[i] = grade[i]/max*100;
-            sum +=grade[i];
-        }
-        System.out.println(sum/N);
-     }
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int max = 0;
+		int arr[] = new int[n];
+		float sum = 0.0f;
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+			
+			if(arr[i]>max)
+				max = arr[i];
+		}
+		
+		for(int i=0; i<n; i++) {
+			sum += ((float)arr[i]/max)*100;
+		}
+		System.out.println(sum / n);
+		
+	}
 }
