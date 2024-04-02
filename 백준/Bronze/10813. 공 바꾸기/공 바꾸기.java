260c29e01e85d003e3bd56c35e3e87da0931230f
RangKoon <162738215+RangKoon@users.noreply.github.com>
2024-04-02 20:44:29 +0900

    [Bronze II] Title: 공 바꾸기, Time: 124 ms, Memory: 13072 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,26 +1,31 @@
-import java.util.*;
-public class Main{
-
-     public static void main(String []args){
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int tmp = 0;
-        int[] ball = new int[n];
-        
-        for(int i = 0; i<n; i++){
-          ball[i] = i+1;
-        }
-        for(int i = 0; i<m; i++){
-          int a = sc.nextInt();
-          int b = sc.nextInt();
-          tmp = ball[a-1];
-          ball[a-1] = ball[b-1];
-          ball[b-1] = tmp;
-          }
-        for(int i=0; i<n; i++){
-          System.out.print(ball[i]+ " ");
-        }
-     }
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int arr[] = new int[n];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = i+1;
+		}
+		
+		for(int i=0; i<m; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int tmp = 0;
+			
+			tmp = arr[a-1];
+			arr[a-1] = arr[b-1];
+			arr[b-1] = tmp;
+			
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+	}
 }
