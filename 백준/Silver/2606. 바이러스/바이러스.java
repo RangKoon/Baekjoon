7ef817331899abf8c2c4ec3bdd9d8df1c6226d88
RangKoon <162738215+RangKoon@users.noreply.github.com>
2024-07-01 13:33:40 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -3,61 +3,42 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-        
-        for(int i=0; i<n+1; i++) {
-        	ArrayList<Integer> newarr = new ArrayList<>();
+        int t = Integer.parseInt(br.readLine());
+        LinkedList<LinkedList<Integer>> arr = new LinkedList<>();
+        for(int i=0; i<=n; i++) {
+        	LinkedList<Integer> newarr = new LinkedList<>();
         	arr.add(newarr);
         }
-        
-        for(int i=0; i<m; i++) {
+        for(int i=0; i<t; i++) {
         	String[] input = br.readLine().split(" ");
         	int a = Integer.parseInt(input[0]);
         	int b = Integer.parseInt(input[1]);
         	arr.get(a).add(b);
         	arr.get(b).add(a);
         }
-        
-        int cnt = 0;
-        int start = 1;
-        boolean[] visited = new boolean[n+1]; // 방문 확인 배열
-        
         Queue<Integer> q = new LinkedList<>();
-        
-        q.add(start); 
-        visited[start] = true;
-        
-        // start의 인접 리스트 q에 삽입
-        for(int i=0; i<arr.get(start).size(); i++) { 
-        	int tmp = arr.get(start).get(i);
-        	q.add(tmp);
-        	visited[tmp] = true;
-        	cnt ++;
+        boolean[] visited = new boolean[n+1];
+        q.add(1);
+        visited[1] = true;
+        for(int i=0; i<arr.get(1).size(); i++) {
+        	q.add(arr.get(1).get(i));
+        	visited[arr.get(1).get(i)] = true;
         }
         q.remove();
-        
+        int cnt = 0;
         while(q.size() != 0) {
-        	
-        	int t = q.peek();
-        	
-        	for(int i=0; i<arr.get(t).size(); i++) {
-        		int tmp2 = arr.get(t).get(i);
-        		if(visited[tmp2])
-        			continue;
-        		else {
-        			q.add(tmp2);
-        			visited[tmp2] = true;
-        			cnt ++;
+        	cnt ++;
+        	int tmp = q.peek();
+        	for(int i=0; i<arr.get(tmp).size(); i++) {
+        		if(visited[arr.get(tmp).get(i)] == false ) {
+        			q.add(arr.get(tmp).get(i));
+        			visited[arr.get(tmp).get(i)] = true;
+        			
         		}
-        		
         	}
-        	
         	q.remove();
         }
-        
         System.out.println(cnt);
         
         
